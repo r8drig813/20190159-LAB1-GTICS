c936811721066075d6f0de70f46ba93e26333238
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2023-08-24 20:43:48 -0500

    Primer commit

---
+++ lab1-gtics/src/test/java/com/example/lab1gtics/Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
